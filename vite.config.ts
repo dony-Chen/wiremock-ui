cca4ef920d2eab4eb1f159a236c9c4f5442781b4
dony-Chen <jerrybearooxx05@gmail.com>
2024-03-18 10:30:06 +0800

    03_18_ver

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue(), vueJsx()],
-    // base: './',           //域名部署，即： http://localhost:5173/
-    base: '/wiremock',   //二级目录部署，即：http://localhost:5173/wiremock
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: { mode: any }) => {
+    //process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+    const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
+    return defineConfig({
+        server: {
+            port: port,
+            host: process.env.VITE_HOST,
+        },
+        plugins: [vue(), vueJsx()],
+        // base: './',           //..................... http://localhost:5173/
+        base: '/wiremock',   //...........................http://localhost:5173/wiremock
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
         }
-    }
-})
+    })
+}
